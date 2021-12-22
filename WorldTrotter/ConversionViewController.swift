2ac2954a033fb2ddfc8af55ff109ed75d3acde19
dcen7 <mdcengiz@gmail.com>
2021-12-22 07:33:52 -0600

    add segmented control programmatically

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Mehmet Deniz Cengiz on 12/12/21.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Conversion loaded its view")
+    }
+    
+
+}
+
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Mehmet Deniz Cengiz on 12/21/21.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Map loaded its view")
+    }
+    
+
+}
