29b6f3759420fbc9e861cf3eedc8d32acf1b5b6c
dcen7 <mdcengiz@gmail.com>
2021-12-27 12:39:12 -0600

    respond text field changes

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("Conversion loaded its view")
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text  = textField.text , !text.isEmpty{
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+        
     }
     
 
